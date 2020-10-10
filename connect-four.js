64cbc4d78bc3be5ecd9b58d9fdff20bcadfc7627
Joshua <j.maxwell.alan@gmail.com>
2020-10-10 02:14:56 -0400

    Started minifying connect-four

--- connect-four.js
+++ connect-four.js
@@ -1,16 +1,16 @@
 import { e } from './e.js';
-let game;
-const gameName = document.getElementById('game-name');
-function updateUI(){
+let a;
+const b = document.getElementById('game-name');
+function c(){
     const board = document.getElementById('board-holder');
-    if(!game){
+    if(!a){
         board.classList.add('is-invisible');
     }else{
         board.classList.remove('is-invisible');
-        gameName.innerHTML = game.j();
+        b.innerHTML = a.j();
         for(let columnIdx = 0; columnIdx <= 6; columnIdx++){
 
-            if(game.l(columnIdx)){
+            if(a.l(columnIdx)){
                 document.getElementById(`column-${columnIdx}`).classList.add('full');
             }else{
                 document.getElementById(`column-${columnIdx}`).classList.remove('full');
@@ -21,7 +21,7 @@ function updateUI(){
         for(let columnIdx = 0; columnIdx <= 6; columnIdx++){
             const square = document.getElementById(`square-${rowIdx}-${columnIdx}`);
             square.innerHTML = '';
-            const playerNumber = game.k(rowIdx,columnIdx);
+            const playerNumber = a.k(rowIdx,columnIdx);
             if(playerNumber === 1){
                 const tokenDiv = document.createElement('div');
                 tokenDiv.classList.add('black', 'token');
@@ -34,7 +34,7 @@ function updateUI(){
         }
     }
     const clickTargets = document.getElementById('click-targets');
-    const currentPlayer = game.c;
+    const currentPlayer = a.c;
     if (currentPlayer === 1) {
         clickTargets.classList.add('black');
         clickTargets.classList.remove('red');
@@ -60,18 +60,18 @@ window.addEventListener('DOMContentLoaded', event => {
     })
     newGame.addEventListener('click', event =>{
 
-        game = new e(playerOne.value, playerTwo.value);
-        updateUI();
+        a = new e(playerOne.value, playerTwo.value);
+        c();
     })
     const clickTargets = document.getElementById('click-targets');
     clickTargets.addEventListener('click', event => {
         const targetId = event.target.id;
         const columnIdx = Number.parseInt(targetId.split('column-').join(''));
 
-        if(game.l(columnIdx)) return;
+        if(a.l(columnIdx)) return;
 
         if(!targetId.startsWith('column-')) return;
-        game.i(columnIdx);
-        updateUI();
+        a.i(columnIdx);
+        c();
     });
 });
