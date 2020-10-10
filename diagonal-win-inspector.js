986271a294b0953bffac3cd9101143ded04bec3a
Joshua <j.maxwell.alan@gmail.com>
2020-10-09 21:02:45 -0400

    Further cleaned up row win

--- diagonal-win-inspector.js
+++ diagonal-win-inspector.js
@@ -1,31 +1,12 @@
 export class DiagonalWinInspector {
     static inspect(columns) {
-        let column1 = columns[0];
-        let column2 = columns[1];
-        let column3 = columns[2];
-        let column4 = columns[3];
+        let winCheck;
+        let tokenArry = [];
         for (let i = 0; i < 3; i++) {
-            let currentTokenCol1DownSlant = column1.getTokenAt(i);
-            let currentTokenCol2DownSlant = column2.getTokenAt(i+1);
-            let currentTokenCol3DownSlant = column3.getTokenAt(i+2);
-            let currentTokenCol4DownSlant = column4.getTokenAt(i+3);
-            let currentTokenCol1UpSlant = column1.getTokenAt(i+3);
-            let currentTokenCol2UpSlant = column2.getTokenAt(i+2);
-            let currentTokenCol3UpSlant = column3.getTokenAt(i+1);
-            let currentTokenCol4UpSlant = column4.getTokenAt(i);
-            if (currentTokenCol1DownSlant === currentTokenCol2DownSlant &&
-                currentTokenCol1DownSlant === currentTokenCol3DownSlant &&
-                currentTokenCol1DownSlant === currentTokenCol4DownSlant &&
-                currentTokenCol1DownSlant !== null) {
-                    return currentTokenCol1DownSlant;
-
-            } else if (currentTokenCol1UpSlant === currentTokenCol2UpSlant &&
-                currentTokenCol1UpSlant === currentTokenCol3UpSlant &&
-                currentTokenCol1UpSlant === currentTokenCol4UpSlant &&
-                currentTokenCol1UpSlant !== null) {
-                    return currentTokenCol1UpSlant;
-            }
-        }
-        return 0;
+        columns.forEach((column, idx) => {
+            tokenArry.push(column.getTokenAt(i + idx))
+        });
     }
+        return 0;
+        }
 }
--- row-win-inspector.js
+++ row-win-inspector.js
@@ -4,10 +4,10 @@ export class RowWinInspector {
         for (let i = 0; i < 6; i++) {
             let currentTokens = [];
             columns.forEach(column => { currentTokens.push(column.getTokenAt(i)) });
-            if (((currentTokens.reduce((acc, token) => acc + token) / 4) === 1 ||
-                (currentTokens.reduce((acc, token) => acc + token) / 4) === 2) &&
-                currentTokens.every(token => token)) {
-                    winCheck = currentTokens[0];
+            const playerInRow = (currentTokens.reduce((acc, token) => acc + token) / 4);
+            const playerExists = currentTokens.every(token => token);
+            if (playerExists && (playerInRow === 1 || playerInRow === 2)) {
+                    winCheck = playerInRow;
             }
         }
         if (winCheck) return winCheck;
