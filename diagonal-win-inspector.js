f9d0063f7ea5c62165b8168f601f48e6d83a148b
Joshua <j.maxwell.alan@gmail.com>
2020-10-10 00:00:52 -0400

    Got diagonal win condition working with arrays

--- diagonal-win-inspector.js
+++ diagonal-win-inspector.js
@@ -1,28 +1,19 @@
 export class DiagonalWinInspector {
     static inspect(columns) {
-        let tokenDownArray = [];
-        let tokenUpArray = [];
-        let winCheck = 0;
-        for (let i = 0; i < 3; i++) {
+        let downSlantTokens = [];
+        let upSlantTokens = [];
+        for (let i = 0; i < 3; i ++) {
             columns.forEach((column, idx) => {
-                console.log(`Token at column ${idx} and index ${i + idx} is ${column.tokens[i + idx]}, pushed to tokenDownArray`)
-                tokenDownArray.push(column.tokens[i + idx]);
-                console.log(`Token at column ${idx} and index ${i + (3 - idx)} is ${column.tokens[i + (3 - idx)]}, pushed to tokenUpArray`)
-                tokenUpArray.push(column.tokens[i + (3 - idx)]);
+                downSlantTokens.push(column.getTokenAt(i+idx));
+                upSlantTokens.push(column.getTokenAt(i + (3 - idx)));
             });
-            console.log('Upward tokens after forEach',tokenUpArray);
-            console.log('Downward tokens after forEach',tokenDownArray);
-            tokenDownArray = tokenDownArray.filter(token => token);
-            tokenUpArray = tokenUpArray.filter(token => token);
-            console.log('Upward tokens after filter',tokenUpArray);
-            console.log('Downward tokens after filter',tokenDownArray);
-            const playerDownCanWin = tokenDownArray.length === 4;
-            const playerUpCanWin = tokenUpArray.length === 4;
-            const playerDownHasWon = tokenDownArray.every((token,x,arry) => token === arry[0]);
-            const playerUpHasWon = tokenDownArray.every((token,x,arry) => token === arry[0]);
-            if (playerUpCanWin && playerUpHasWon) return winCheck = tokenUpArray.reduce((acc, token) => acc + token);
-            if (playerDownCanWin && playerDownHasWon) return winCheck = tokenDownArray.reduce((acc, token) => acc + token);
+            const downSlantHasWon = (downSlantTokens.every((token,x,arry) => token === arry[0])) && downSlantTokens.every(token => token);
+            const upSlantHasWon = (upSlantTokens.every((token,x,arry) => token === arry[0])) && upSlantTokens.every(token => token);
+            if (downSlantHasWon) return downSlantTokens[0];
+            if (upSlantHasWon) return upSlantTokens[0];
+            downSlantTokens = [];
+            upSlantTokens = [];
         }
-        return winCheck;
+        return 0;
     }
 }
